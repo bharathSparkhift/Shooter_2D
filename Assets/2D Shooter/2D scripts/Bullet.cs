7dc347cfb89d16110fb5321a799c18a58c3f2698
bharath_adcrack <bharath@adcrack.com>
2024-07-09 18:24:00 +0530

    Bullet reset its position when it gets out of the boundary horizontally

--- Assets/2D Shooter/2D scripts/Bullet.cs
+++ Assets/2D Shooter/2D scripts/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine.XR;
 
 public class Bullet : MonoBehaviour
 {
-
+    [SerializeField] RectTransform gameCanvas;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] RectTransform rectTransform;
     [SerializeField] float forceSpeed = 2f;
@@ -23,40 +23,31 @@ public class Bullet : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         DisableGameObject();
-        if (collision.collider.tag == "Obstacle")
-        {
-            
-            // Debug.Log($"{nameof(OnCollisionEnter2D)} \t {collision.gameObject.name}");
-        }
-        
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "Game Canvas")
-        {
-            // DisableGameObject();
-            // Debug.Log($"{nameof(OnCollisionEnter2D)} \t {collision.gameObject.name}");
-        }
-    }
+   
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
-    }
-
+  
     private void OnEnable()
     {
         
         Vector2 upwardWorldDirection = transform.TransformDirection(transform.up);
         rb.AddForce(upwardWorldDirection * forceSpeed, ForceMode2D.Impulse);
         rectTransform.localRotation = Quaternion.Euler(0, 0, 0);
-        // Invoke(nameof(DisableGameObject), disableDelay);
+
     }
 
     void DisableGameObject()
     {
         rectTransform.anchoredPosition = Vector3.zero;
         gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(rectTransform.anchoredPosition.x < -gameCanvas.sizeDelta.x/2 || rectTransform.anchoredPosition.x > gameCanvas.sizeDelta.x / 2)
+        {
+            DisableGameObject();
+        }
     }
 }
