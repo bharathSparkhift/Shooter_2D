886723aa1aadab53b6386f204d2fda3be44a2d0a
bharath_adcrack <bharath@adcrack.com>
2024-07-12 18:54:04 +0530

    PlayerData.cs model has to be updated

--- Assets/2D Shooter/2D scripts/PlayerData.cs
+++ Assets/2D Shooter/2D scripts/PlayerData.cs
@@ -31,6 +31,12 @@ public class PlayerData : MonoBehaviour
     class Report
     {
         public string UserName;
+        public List<Data> AllData  = new List<Data>();
+    }
+
+    [Serializable] 
+    class Data
+    {
         public string Score;
         public string PlayTime;
         public string StartDateTime;
@@ -61,6 +67,8 @@ public class PlayerData : MonoBehaviour
 
     Report report;
 
+    Data cachedData;
+
     [HideInInspector]
     public Login login;
 
@@ -81,29 +89,27 @@ public class PlayerData : MonoBehaviour
     private void OnDisable()
     {
         OnPlayerData -= UpdatePlayerCollection;
-        
     }
-
-   
 
     // Start is called before the first frame update
     void Start()
     {
         report = new Report();
+        cachedData = new Data();
         login = new Login();
         PlayerData_Wrapper = new PlayerDataWrapper();
         Collect_Item = new CollectItem();
         report = ReadDataFromLocalFile();
-        report.StartDateTime = DateTime.Now.ToString();
+        //report.StartDateTime = DateTime.Now.ToString();
         Debug.Log($"File path \t {FilePath}");
     }
 
     private void OnApplicationQuit()
     {
-        report.EndDateTime = DateTime.Now.ToString();
-        var timeDiff = (DateTime.Now - DateTime.Parse(report.StartDateTime));
-        report.PlayTime = timeDiff.ToString();
-        _reportContent = JsonConvert.SerializeObject(report);
+        //report.EndDateTime = DateTime.Now.ToString();
+        //var timeDiff = (DateTime.Now - DateTime.Parse(report.StartDateTime));
+        //report.PlayTime = timeDiff.ToString();
+        _reportContent = JsonConvert.SerializeObject(report, Formatting.Indented);
         SaveDataToLocalFile();
         Debug.Log($"<color=orange>{nameof(OnApplicationQuit)}</color> \t Report {_reportContent}");
     }
@@ -119,18 +125,21 @@ public class PlayerData : MonoBehaviour
         Collect_Item.Count += 1;
         PlayerData_Wrapper.CollectItems.Add(Collect_Item);
 
-        int value = 0;
-        if (!report.ItemCollected.ContainsKey(obstacleName))
-        {
-            report.ItemCollected.Add(obstacleName, 0);
-        }
-        
-        report.ItemCollected.TryGetValue(obstacleName, out value);
-        value += 1;
-        report.ItemCollected[obstacleName] = value;
+        /*int value = 0;
 
-        _reportContent = JsonConvert.SerializeObject(report); // report.
-        SaveDataToLocalFile();
+        cachedData = report.AllData[report.AllData.Count - 1];
+        if (!cachedData.ItemCollected.ContainsKey(obstacleName))
+        {
+            cachedData.ItemCollected.Add(obstacleName, 0);
+        }
+
+        cachedData.ItemCollected.TryGetValue(obstacleName, out value);
+        value += 1;
+        cachedData.ItemCollected[obstacleName] = value;
+        report.AllData[report.AllData.Count - 1] = cachedData;
+
+        _reportContent = JsonConvert.SerializeObject(report);*/
+
         Debug.Log($"Update player collection {_reportContent}");
     }
 
@@ -156,53 +165,51 @@ public class PlayerData : MonoBehaviour
             report = new Report
             {
                 UserName = string.Empty,
-                Score = "0",
-                PlayTime = string.Empty,
-                StartDateTime = string.Empty,
-                ItemCollected = new Dictionary<string, int>
+            };
+            AddDataToList();
+        }
+        else
+        {
+            try
+            {
+                
+                // Read data from the existing file
+                string fileContent = File.ReadAllText(FilePath);
+                if (!string.IsNullOrEmpty(fileContent))
+                {
+                    report = JsonConvert.DeserializeObject<Report>(fileContent);
+                    // Add data to the list
+                    // AddDataToList();
+                }
+            }
+            catch (IOException ex)
+            {
+                Debug.LogError("IOException: " + ex.Message);
+            }
+            Debug.Log($"{nameof(ReadDataFromLocalFile)} \n File path {FilePath} \n {JsonConvert.SerializeObject(report)}");
+        }
+        return report;
+    }
+
+    void AddDataToList()
+    {
+        report.AllData.Add(new Data()
+        {
+            Score = "0",
+            PlayTime = string.Empty,
+            StartDateTime = string.Empty,
+            ItemCollected = new Dictionary<string, int>
             {
                 { Obstacle.Type.square.ToString(), 0 },
                 { Obstacle.Type.circle.ToString(), 0 },
                 { Obstacle.Type.triangle.ToString(), 0 },
                 { Obstacle.Type.diamond.ToString(), 0 }
             }
-            };
-            _reportContent = JsonConvert.SerializeObject(report);
-            try
-            {
-                // Create the file and write initial data
-                using (FileStream fileStream = File.Create(FilePath))
-                {
-                    using (StreamWriter writer = new StreamWriter(fileStream))
-                    {
-                        writer.Write(_reportContent);
-                    }
-                }
-            }
-            catch (IOException ex)
-            {
-                Debug.LogError("IOException: " + ex.Message);
-            }
-            Invoke(nameof(SaveDataToLocalFile), 1f);
-        }
-        else
-        {
-            try
-            {
-                // Read data from the existing file
-                string fileContent = File.ReadAllText(FilePath);
-                if (!string.IsNullOrEmpty(fileContent))
-                {
-                    report = JsonConvert.DeserializeObject<Report>(fileContent);
-                }
-            }
-            catch (IOException ex)
-            {
-                Debug.LogError("IOException: " + ex.Message);
-            }
-            Debug.Log($"{nameof(ReadDataFromLocalFile)} \n File path {FilePath}");
-        }
-        return report;
+        });
+        _reportContent = JsonConvert.SerializeObject(report);
+        
+        SaveDataToLocalFile();
+        
     }
 
     public void LoginOnButtonClick(string username)
